f6db443c4b1d70a38807e25f096cb31d854659d9
kbl212 <kyle.lauritzen@gmail.com>
2016-03-31 02:13:50 -0600

    fully implemented contact page. email form complete, with validation

--- public/features/contactPage/contactCtrl.js
+++ public/features/contactPage/contactCtrl.js
@@ -22,4 +22,25 @@ app.controller('contactCtrl', function ($scope) {
     }
 
     $scope.initMap();
+
+    $scope.isValidEmail = false;
+
+    $scope.isEmail = function (email) {
+        var returnVal = /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*$/.test(email);
+        $scope.isValidEmail = returnVal;
+        return returnVal;
+    }
+
+    $scope.emailSubmit = function() {
+        console.log('HERE');
+        if ($scope.emailName && $scope.isValidEmail && $scope.emailSubject && $scope.emailMessage) {
+            alert("SUCCESS!");
+        }
+        else if (!$scope.isValidEmail) {
+            alert("Please enter a valid email address");
+        }
+        else {
+            alert("Please fill out all required fields");
+        }
+    }
 });
