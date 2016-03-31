babd2c67b293297e57bac554790ddfdaa3b2b411
kbl212 <kyle.lauritzen@gmail.com>
2016-03-30 22:25:37 -0600

    working on contact page. also fixed allWork image/thumbnail layout to a side-by-side view

--- public/features/contactPage/contactCtrl.js
+++ public/features/contactPage/contactCtrl.js
@@ -1,6 +1,25 @@
 var app = angular.module('intermountain-lift');
 
 
-app.controller('contactCtrl', function($scope) {
-    
+app.controller('contactCtrl', function ($scope) {
+
+    $scope.initMap = function () {
+        var myLatLng = {
+            lat: 40.140210,
+            lng: -111.614170
+        };
+
+        $scope.map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 16,
+            center: myLatLng
+        });
+
+        var marker = new google.maps.Marker({
+            position: myLatLng,
+            map: $scope.map,
+            title: 'intermountainLift'
+        });
+    }
+
+    $scope.initMap();
 });
