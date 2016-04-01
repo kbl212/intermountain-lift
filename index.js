7786411aca5680111d3fee9cb5c06e27d3429cd4
kbl212 <kyle.lauritzen@gmail.com>
2016-03-31 20:05:23 -0600

    added access headers to node...

--- index.js
+++ index.js
@@ -5,6 +5,24 @@ var bodyParser = require('body-parser');
 var app = express();
 
 app.use(cors());
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://www.lauritzentest.herokuapp.com');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/public'));
