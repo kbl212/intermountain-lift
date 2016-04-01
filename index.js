0571c8d1b6271c349124d936cddb253047d49c71
kbl212 <kyle.lauritzen@gmail.com>
2016-03-31 20:59:42 -0600

    trying one of two things server side...

--- index.js
+++ index.js
@@ -4,7 +4,32 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
-app.use(cors());
+app.use(function(req, res, next) {
+    var oneof = false;
+    if(req.headers.origin) {
+        res.header('Access-Control-Allow-Origin', req.headers.origin);
+        oneof = true;
+    }
+    if(req.headers['access-control-request-method']) {
+        res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
+        oneof = true;
+    }
+    if(req.headers['access-control-request-headers']) {
+        res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
+        oneof = true;
+    }
+    if(oneof) {
+        res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365);
+    }
+
+    // intercept OPTIONS method
+    if (oneof && req.method == 'OPTIONS') {
+        res.send(200);
+    }
+    else {
+        next();
+    }
+});
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
