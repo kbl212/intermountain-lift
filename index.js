980264e61c5c48c8c3ccd28178faa0ea9baaf0ab
kbl212 <kyle.lauritzen@gmail.com>
2016-03-31 20:08:56 -0600

    modified header in node...

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://www.lauritzentest.herokuapp.com');
+    res.setHeader('Access-Control-Allow-Origin', 'http://lauritzentest.herokuapp.com');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
