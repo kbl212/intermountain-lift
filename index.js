58c0c7a18c1815c07aff1a61cabdd4bbbd41c314
kbl212 <kyle.lauritzen@gmail.com>
2016-03-31 20:18:58 -0600

    trying cors instead...

--- index.js
+++ index.js
@@ -4,26 +4,7 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
-
-app.use(cors());
+app.use(cors({origin: 'http://localhost:3000'}, {origin:'http://lauritzentest.herokuapp.com'}));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
